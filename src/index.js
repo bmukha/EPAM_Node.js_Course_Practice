4a1ce3feca831f81b2a66e1510dc74614ada6096
Bohdan Mukha <by.muha@gmail.com>
2023-09-29 17:41:31 +0300

    Add health-check route

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ const PORT = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/health-check', (req, res) => res.json({ isRunning: true }));
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
