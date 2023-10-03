6aaaa47d7717c7977ef60ab2b2f3c6e1f3107ee9
Bohdan Mukha <by.muha@gmail.com>
2023-10-04 01:58:25 +0300

    Convert index to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express, { NextFunction, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerJsDoc, { Options } from 'swagger-jsdoc';
 
 import healthCheckRouter from './routes/health-check.js';
 import dadJokeRouter from './routes/dad-joke.js';
 
-const swaggerOptions = {
+const swaggerOptions: Options = {
   definition: {
     openapi: '3.0.0',
     info: {
@@ -27,7 +27,7 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+app.get('/', (req: Request, res: Response) => res.send('Hello, World!'));
 
 app.use('/health-check', healthCheckRouter);
 
@@ -35,10 +35,12 @@ app.use('/dad-joke', dadJokeRouter);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.use((req, res, next) => res.status(404).send('404 Not Found'));
+app.use((req: Request, res: Response, next: NextFunction) =>
+  res.status(404).send('404 Not Found')
+);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) =>
   res.status(500).send('Internal Server Error')
 );
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, (): void => console.log(`Server listening on port ${PORT}`));
