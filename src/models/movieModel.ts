c672514995a6b13c4f0296ada980c53282ba1a83
Bohdan Mukha <by.muha@gmail.com>
2023-10-13 16:47:19 +0300

    Add Movie model

---
+++ src/models/movieModel.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const MovieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  release_date: { type: Date, required: true },
+  genres: { type: [mongoose.Schema.Types.ObjectId], ref: 'Genre' },
+});
+
+export const GenreModel = mongoose.model('Movie', MovieSchema);
