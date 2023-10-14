bb419cc2b1772993bc3cb41fa44f32f36865ca52
Bohdan Mukha <by.muha@gmail.com>
2023-10-14 22:10:07 +0300

    Update genres controller to use handler factory

--- src/controllers/genresController.ts
+++ src/controllers/genresController.ts
@@ -1,82 +1,13 @@
-import { Request, Response } from 'express';
-
 import { GenreModel } from '../models/genreModel.ts';
 
-export const getAllGenres = async (req: Request, res: Response) => {
-  try {
-    const response = await GenreModel.find();
-    res.send(response);
-  } catch (error) {
-    res.status(505).send('Genres loading failed');
-  }
-};
+import * as handlerFactory from '../utils/handlerFactory.ts';
 
-export const getGenreById = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const response = await GenreModel.findById(id);
-    res.send(response);
-  } catch (error) {
-    res.status(505).send('Genres loading failed');
-  }
-};
+export const getAllGenres = handlerFactory.getAll(GenreModel);
 
-export const createGenre = async (req: Request, res: Response) => {
-  const {
-    body: { name },
-  } = req;
+export const getGenre = handlerFactory.getOneById(GenreModel);
 
-  try {
-    await GenreModel.create({ name });
+export const createGenre = handlerFactory.createOne(GenreModel);
 
-    res.status(201).send(`Successfully created ${name} genre`);
-  } catch (error) {
-    if (error instanceof Error) {
-      const { name, message } = error;
-      res.status(400).send({ name, message });
-      return;
-    }
-    res.status(400).send('Unknown error occured');
-  }
-};
+export const updateGenre = handlerFactory.updateOne(GenreModel);
 
-export const updateGenre = async (req: Request, res: Response) => {
-  const {
-    params: { id },
-    body: { name },
-  } = req;
-
-  console.log({ id, name });
-
-  try {
-    await GenreModel.findByIdAndUpdate(id, { name });
-
-    res.send(`Successfully updated ${name} genre`);
-  } catch (error) {
-    if (error instanceof Error) {
-      const { name, message } = error;
-      res.status(400).send({ name, message });
-      return;
-    }
-    res.status(400).send('Unknown error occured');
-  }
-};
-
-export const deleteGenre = async (req: Request, res: Response) => {
-  const {
-    body: { name },
-  } = req;
-
-  try {
-    await GenreModel.findOneAndDelete({ name });
-
-    res.send(`Successfully deleted ${name} genre`);
-  } catch (error) {
-    if (error instanceof Error) {
-      const { name, message } = error;
-      res.status(400).send({ name, message });
-      return;
-    }
-    res.status(400).send('Unknown error occured');
-  }
-};
+export const deleteGenre = handlerFactory.deleteOne(GenreModel);
