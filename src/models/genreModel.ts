58ef609f7624d9c99cd4517cd9f1c6e8cd71ea2e
Bohdan Mukha <by.muha@gmail.com>
2023-10-14 19:45:38 +0300

    Update genre schema to use relevant error messages

--- src/models/genreModel.ts
+++ src/models/genreModel.ts
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
 
 const GenreSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: {
+    type: String,
+    required: [true, 'Name is required field!'],
+    unique: [true, 'Movie name must be unique!'],
+  },
 });
 
 export const GenreModel = mongoose.model('Genre', GenreSchema);
