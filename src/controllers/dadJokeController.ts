4695240b1883a3dc631fba96dda502fa634a53e3
Bohdan Mukha <by.muha@gmail.com>
2023-10-15 19:08:34 +0300

    Refactor dad joke controller for consistency

--- src/controllers/dadJokeController.ts
+++ src/controllers/dadJokeController.ts
@@ -1,17 +1,24 @@
 import { type Request, type Response } from 'express';
+import { asyncErrorHandler } from '../utils/asyncErrorHandler.ts';
 
-export const getDadJoke = async (req: Request, res: Response) => {
-  try {
-    const response: globalThis.Response = await fetch('https://icanhazdadjoke.com/', {
-      headers: {
-        Accept: 'application/json',
+export const getDadJoke = asyncErrorHandler(
+  async (req: Request, res: Response) => {
+    const response: globalThis.Response = await fetch(
+      'https://icanhazdadjoe.com/',
+      {
+        headers: {
+          Accept: 'application/json',
+        },
       },
-    });
+    );
 
     const { joke } = await response.json();
 
-    res.send(joke);
-  } catch (error: unknown) {
-    res.status(500).send('Joke load failed');
-  }
-};
+    res.status(200).json({
+      status: 'success',
+      data: {
+        joke,
+      },
+    });
+  },
+);
