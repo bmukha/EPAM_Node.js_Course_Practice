c59dbeee01c84f8c7e0e4650e71a082acae9d82e
Bohdan Mukha <by.muha@gmail.com>
2023-10-15 19:28:14 +0300

    Update Movie Schema

--- src/models/movieModel.ts
+++ src/models/movieModel.ts
@@ -1,10 +1,16 @@
-import mongoose from 'mongoose';
+import { Schema, model, InferSchemaType } from 'mongoose';
 
-const MovieSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  release_date: { type: Date, required: true },
-  genres: { type: [mongoose.Schema.Types.ObjectId], ref: 'Genre' },
+const MovieSchema = new Schema({
+  title: { type: String, required: [true, 'Title is required!'] },
+  description: { type: String, required: [true, 'Description is required!'] },
+  release_date: { type: Date, required: [true, 'Date is required!'] },
+  genres: {
+    type: [Schema.Types.ObjectId],
+    ref: 'Genre',
+    required: [true, 'Genres are required!'],
+  },
 });
 
-export const MovieModel = mongoose.model('Movie', MovieSchema);
+export type Movie = InferSchemaType<typeof MovieSchema>;
+
+export const MovieModel = model('Movie', MovieSchema);
