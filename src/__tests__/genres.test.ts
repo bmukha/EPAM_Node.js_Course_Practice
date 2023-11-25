57a657a4162be710478452071de699f8bac77720
Bohdan Mukha <by.muha@gmail.com>
2023-11-25 20:13:23 +0200

    Add rest of the tests for Genres endpoint

--- src/__tests__/genres.test.ts
+++ src/__tests__/genres.test.ts
@@ -1,16 +1,24 @@
 import request from 'supertest';
 import server from '../server.ts';
 import { connectToDB, disconnectFromDB, clearDB } from '../db/db.ts';
-import { GenreModel } from '../models/genreModel.ts';
+import { Genre, GenreModel } from '../models/genreModel.ts';
 import { genres } from '../data/data.ts';
+
+let firstGenre: Genre & { _id: string };
+let id: string;
 
 beforeAll(async () => {
   await connectToDB(process.env.TEST_DB_URL as string);
 });
+
 beforeEach(async () => {
   await clearDB();
   await GenreModel.create(genres);
+  const response = await request(server).get('/api/v1/genres');
+  firstGenre = response.body.data.genres[0];
+  id = firstGenre['_id'];
 });
+
 afterAll(async () => {
   await disconnectFromDB();
   server.close();
@@ -54,17 +62,92 @@ describe('Genres Endpoint', () => {
         message: expect.stringMatching('duplicate key error'),
       });
     });
+
+    it(`should return status: "fail", code 400 and error message if name is not provided`, async () => {
+      const response = await request(server).post('/api/v1/genres').send();
+
+      expect(response.status).toBe(400);
+      expect(response.body).toMatchObject({
+        status: 'fail',
+        message: expect.stringMatching(
+          'Genre validation failed: name: Name is required!',
+        ),
+      });
+    });
+  });
+});
+describe('Single genre endpoint', () => {
+  describe('GET request', () => {
+    it('should return status: "success" and genre if valid id is provided', async () => {
+      const response = await request(server).get(`/api/v1/genres/${id}`);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject({
+        status: 'success',
+        data: {
+          genre: expect.objectContaining(firstGenre),
+        },
+      });
+    });
+
+    it('should return status: "fail", code 404 and error message if invalid id is provided', async () => {
+      const id = '612d6b59e34dea61d8983e1e';
+      const response = await request(server).get(`/api/v1/genres/${id}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body).toMatchObject({
+        status: 'fail',
+        message: `Genre with id ${id} is not found!`,
+      });
+    });
   });
 
-  it(`should return status: "fail", code 400 and error message if name is not provided`, async () => {
-    const response = await request(server).post('/api/v1/genres').send();
+  describe('PATCH request', () => {
+    it('should return status: "success" and updated genre if valid id is provided', async () => {
+      const response = await request(server)
+        .patch(`/api/v1/genres/${id}`)
+        .send({ name: 'opera' });
 
-    expect(response.status).toBe(400);
-    expect(response.body).toMatchObject({
-      status: 'fail',
-      message: expect.stringMatching(
-        'Genre validation failed: name: Name is required!',
-      ),
+      expect(response.status).toBe(200);
+      expect(response.body).toMatchObject({
+        status: 'success',
+        data: {
+          genre: expect.objectContaining({ name: 'opera' }),
+        },
+      });
+    });
+
+    it('should return status: "fail", code 404 and error message if invalid id is provided', async () => {
+      const id = '612d6b59e34dea61d8983e1e';
+      const response = await request(server)
+        .patch(`/api/v1/genres/${id}`)
+        .send({ name: 'drama' });
+
+      expect(response.status).toBe(404);
+      expect(response.body).toMatchObject({
+        status: 'fail',
+        message: `Genre with id ${id} is not found!`,
+      });
+    });
+  });
+
+  describe('DELETE request', () => {
+    it('should return status: "success" and null if valid id is provided', async () => {
+      const response = await request(server).delete(`/api/v1/genres/${id}`);
+
+      expect(response.status).toBe(204);
+      expect(response.body).toMatchObject({});
+    });
+
+    it('should return status: "fail", code 404 and error message if invalid id is provided', async () => {
+      const id = '612d6b59e34dea61d8983e1e';
+      const response = await request(server).delete(`/api/v1/genres/${id}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body).toMatchObject({
+        status: 'fail',
+        message: `Genre with id ${id} is not found!`,
+      });
     });
   });
 });
